6acb4c8adb9a32d3b9a9122c8f573045e22060e4
stefanoska <75374281+stefanoska@users.noreply.github.com>
2023-05-08 12:25:17 +0300

    aaa

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 # BEGIN CODE HERE
-from flask import Flask
+from ssl import Options
+from flask import Flask, jsonify, request
 from flask_pymongo import PyMongo
 from flask_cors import CORS
 from pymongo import TEXT
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 # END CODE HERE
 
 app = Flask(__name__)
@@ -15,14 +18,27 @@ mongo.db.products.create_index([("name", TEXT)])
 @app.route("/search", methods=["GET"])
 def search():
     # BEGIN CODE HERE
-    return ""
+    list_of_products=[]
+    name = request.args.get("name")
+    productsArray = mongo.db.products.find({"$text": {"$search": f"\"{name}\""}}).toArray()
+    productsArray = sorted(productsArray, key=lambda k: k['price'], reverse=True)
+    for x in productsArray:
+        list_of_products.append(x)
+    return jsonify(list_of_products)
+    #https://www.digitalocean.com/community/tutorials/python-check-if-string-contains-another-string
     # END CODE HERE
 
 
 @app.route("/add-product", methods=["POST"])
 def add_product():
     # BEGIN CODE HERE
-    return ""
+    new_product = request.json
+    exists = mongo.db.products.findOne({"name": new_product["name"]})
+    if exists is not None:
+        mongo.db.products.update_many({"name": new_product["name"]}, {"$set": {"price": new_product["price"], "production_year": new_product["production_year"], "color": new_product["color"], "size": new_product["size"]}})
+    else:
+        mongo.db.products.insert_one(new_product)
+    return 
     # END CODE HERE
 
 
@@ -36,5 +52,35 @@ def content_based_filtering():
 @app.route("/crawler", methods=["GET"])
 def crawler():
     # BEGIN CODE HERE
-    return ""
+    try:
+        semester = request.json
+        url = "https://qa.auth.gr/el/x/studyguide/600000438/current"
+        options = Options()
+        options.headless = True
+        driver = webdriver.Chrome(options=options)
+        driver.get(url)
+        if semester==1:
+            elements = driver.find_elements(By.ID, "exam1")
+        elif semester==2:
+            elements = driver.find_elements(By.ID, "exam2")
+        elif semester==3:
+            elements = driver.find_elements(By.ID, "exam3")
+        elif semester==4:
+            elements = driver.find_elements(By.ID, "exam4")
+        elif semester==5:
+            elements = driver.find_elements(By.ID, "exam5")
+        elif semester==6:
+            elements = driver.find_elements(By.ID, "exam6")
+        elif semester==7:
+            elements = driver.find_elements(By.ID, "exam7")
+        elif semester==8:
+            elements = driver.find_elements(By.ID, "exam8")
+        element = elements.find_elements(By.TAG_NAME, "a")    
+        res = []
+        for e in element:
+            res.append(e.text)
+        return jsonify(res),200
+    except Exception as e:
+        return "BAD REQUEST", 400
+    
     # END CODE HERE
