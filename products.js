62e26dbac633a87f936986722cfe6e0e3b366b37
stefanoska <75374281+stefanoska@users.noreply.github.com>
2023-05-08 14:37:47 +0300

    c

--- products.js
+++ products.js
@@ -2,13 +2,34 @@ const api = "http://127.0.0.1:5000";
 
 window.onload = () => {
     // BEGIN CODE HERE
-
+    const getSearchButton = document.getElementById("search");
+    getSearchButton.onclick = searchButtonOnClick;
     // END CODE HERE
 }
 
 searchButtonOnClick = () => {
     // BEGIN CODE HERE
+    const getProduct = document.getElementById("searchInput");
+   
+    const resultsTable = document.getElementById("results");
+    
+    
 
+    var cellCount = resultsTable.rows[0].cel.length;
+
+    //addrow
+    var rowLength = resultsTable.rows.length;
+    var newRow = resultsTable.insertRow(rowLength);
+
+    for(var i=0; i<cellCount; i++){
+        var newCell = newRow.insertCell(i);
+        newRow.cells[0].innerHtml = rowLength;
+    }
+    
+    
+
+    //time.innerHTML = `${getProduct.value}`
+    //resultsDiv.appendChild(time);
     // END CODE HERE
 }
 
